2f7dd17c19a3a21d01ccdec31bab2d770dc36cb2
baris.gungor@etiya.com <baris.gungor@etiya.com>
2022-04-11 11:21:03 +0300

    update/fix

--- css/index.js
+++ css/index.js
@@ -1,6 +1,6 @@
 window.addEventListener("DOMContentLoaded", function () {
   let todolist = [];
-  let i = 0;
+  let i = 0,check=[];
   let list = document.getElementById("tasklist");
   // Loop for localstorage to work nicely
   let count = JSON.parse(localStorage.getItem(localStorage.key(0))).length;
@@ -10,30 +10,31 @@ window.addEventListener("DOMContentLoaded", function () {
     // Make sure that we only read the todolist from local storage
     todolist = JSON.parse(localStorage.getItem("todolist"));
     let item = document.createElement("li");
-    let deletebutton = document.createElement("BUTTON");
-    let tikbox = document.createElement("input");
-    tikbox.type = "checkbox";
-    tikbox.id = "tikbox";
-    tikbox.className = "tikbox";
-    deletebutton.className = "deletebutton";
-    deletebutton.innerHTML = "X";
-    if (todolist[i].jobstatus == true) {
-      tikbox.checked = true;
-    }
-    // if (tikbox.checked == true) {
+    // let deletebutton = document.createElement("BUTTON");
+    // let tikbox = document.createElement("input");
+    // tikbox.type = "checkbox";
+    // tikbox.id = "tikbox";
+    // tikbox.className = "tikbox";
+    // deletebutton.className = "deletebutton";
+    // deletebutton.innerHTML = "X";
+    
+    // if (item[i].onclick == check) {
     //   todolist[i].jobstatus = true;
-    //   console.log(tikbox.checked)
     // }
-    deletebutton.onclick = function () {
-      for (i = 0; i < count; i++) {
-        //if(key(i)== todolist[i].jobname;
-      }
-      return false;
-    };
+    if (todolist[i].jobstatus == true) {
+      item.className = "checked";
+    }
+
+    // deletebutton.onclick = function () {
+    //   for (i = 0; i < count; i++) {
+    //     //if(key(i)== todolist[i].jobname;
+    //   }
+    //   return false;
+    // };
     item.textContent = todolist[i].jobname;
     list.appendChild(item);
-    list.appendChild(deletebutton);
-    list.appendChild(tikbox);
+    // list.appendChild(deletebutton);
+    // list.appendChild(tikbox);
   }
   newElement = function () {
     let newinput = document.getElementsByClassName("name");
